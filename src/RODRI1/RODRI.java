fdf900efac379ede3dfdde4be6299aeff27bf526
CASA <CASA@192.168.1.77>
2022-04-06 14:28:40 -0500

    P9

---
+++ src/RODRI1/RODRI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package RODRI1;
+
+/**
+ *
+ * @author RODRI
+ */
+public class RODRI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
